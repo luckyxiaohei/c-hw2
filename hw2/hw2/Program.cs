b6bcea9f67ae7df8bff5608ac87dfeea1d323556
luckyxiaohei <1440884465@qq.com>
2019-06-10 20:40:39 +0800

    hw2_12

---
+++ hw2/hw2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2
+{
+
+    static class staticClass
+    {
+        public static int SC(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine(args[0]);
+            }
+            else
+            {
+                Console.WriteLine(args[1] + args[2]);
+            }
+            return 0;
+
+        }
+    }
+
+    class noneStatic
+    {
+        public int NSC(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine(args[0]);
+            }
+            else
+            {
+                Console.WriteLine(args[1] + args[3]);
+            }
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            noneStatic nsc = new noneStatic();
+           int a = nsc.NSC(args);
+         
+           int c= staticClass.SC(args);
+            Console.ReadKey();
+        }
+    }
+}
