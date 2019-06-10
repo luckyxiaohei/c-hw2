2c98f36c820ded8f3c40eeb65d38b0ce087c686b
luckyxiaohei <1440884465@qq.com>
2019-06-10 22:28:36 +0800

    hw2_4

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    class hw2_4
+    {
+         public string hw2()
+        {
+            string[] m = Environment.GetCommandLineArgs(); 
+            return m[1];
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hw2_4 hw = new hw2_4();
+           string k = hw.hw2();
+            MessageBox.Show(k);
+        }
+    }
+}
